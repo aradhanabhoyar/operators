6184a179e89ead92e48652c552acb5d04dbd9730
aradhanabhoyar <168920602+aradhanabhoyar@users.noreply.github.com>
2025-06-13 12:33:54 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,360 @@
+#Addition,Subtraction,Multiplication of two data type.
+#int+int(-*)
+a = 2
+b = 6
+print(a + b)
+print(a-b)
+print(a*b)
+
+#str + str(-*)
+a = "Ram"
+b = "Sham"
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float + float(-*)
+a = 20.2
+b = 10.5
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool + bool(-*)
+a = False
+b = False
+print(a+b)
+print(a-b)
+print(a*b)
+
+a = True
+b = True
+print(a+b)
+print(a-b)
+print(a*b)
+
+a = False
+b = True
+print(a+b)
+print(a-b)
+print(a*b)
+
+#complex + complex(-*)
+a = 2 + 4j
+b = 5 + 8j
+print(a+b)
+print(a-b)
+print(a*b)
+
+# list + list(-*)
+a = ["abc",1,5]
+b = ["ravi","sai",2.5]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#tuple + tuple(-*)
+a = (1,2,3)
+b = (4,5,6)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#set + set(-*)
+a = {3,6,2}
+b = {1,8,9}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#dict + dict(-*)
+a = {"d1":2}
+b = {"d2":4}
+print(a+b)
+print(a-b)
+print(a*b)
+
+# int + str(-*)
+a = 5
+b = "10"
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+float(-*)
+a = 4
+b = 3.6
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+bool(-*)
+a = 4
+b = True
+print(a+b)
+print(a-b)
+print(a*b)
+
+a = 4
+b = False
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+complex(-*)
+a = 5
+b = 2 + 2j
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+list(-*)
+a = 4
+b = [1,2,3]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+tuple(-*)
+a = 4
+b = (1,2,3)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+set(-*)
+a = 4
+b = {1,2,3}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#int+dict(-*)
+a = 4
+b = {"d1":1}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+float(-*)
+a = "hello"
+b = 3.2
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+bool(-*)
+a = "abc"
+b = True
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+complex(-*)
+a = "Ram"
+b = 2+3j
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+list(-*)
+a = "hello"
+b = [1,3,2]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+tuple(-*)
+a = "Ram"
+b = (1,"abc")
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+set(-*)
+a = "hello"
+b = {1,2,3}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#str+dict(-*)
+a = "hello"
+b = {"a": 1}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+bool(-*)
+a = 2.3
+b = True
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+complex(-*)
+a = 2.3
+b = 2+3j
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+list(-*)
+a = 2.3
+b = [1,2,3]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+tuple(-*)
+a = 2.3
+b = (1,2)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+set(-*)
+a = 2.1
+b = {1,2}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#float+dict(-*)
+a = 2.3
+b = {"a":1}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool+complex(-*)
+a = True
+b = 2+3j
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool+list(-*)
+a = True
+b = [1,2]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool+tuple(-*)
+a = True
+b = (1,2)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool+set(-*)
+a = True
+b = {1,2}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#bool+dict(-*)
+a = True
+b = {"q":4}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#Complex+list(-*)
+a = 2+3j
+b = [1,2]
+print(a+b)
+print(a-b)
+print(a*b)
+
+#Complex+tuple(-*)
+a = 2+3j
+b = (1,3)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#Complex+set(-*)
+a = 2+3j
+b = {1,2}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#Complex+dict(-*)
+a  = 4+4j
+b = {"a":3}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#list+tuple(-*)
+a = [5,6]
+b = (3,4)
+print(a+b)
+print(a-b)
+print(a*b)
+
+#list+set(-*)
+a =[5,6]
+b = {3,4}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#list+dict(-*)
+a = [5,6]
+b = {"a":4}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#tuple+set(-*)
+a = (5,6)
+b = {3,4}
+print(a+b)
+print(a-b)
+print(a*b)
+
+#tuple+dict(-*)
+a = (1,2)
+b = {"r":3}
+print(a+b)
+print(a-b)
+print(a*b)
+
+print("*"*5)
+
+a = [1,2,3]
+print(a*4)
+
+a = "rakhi"
+print(a*2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
